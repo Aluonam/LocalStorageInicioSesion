76bcb9e91a8dd7799ae246378713c6a1f07eb2b2
Aluon <paulacanoroman@gmail.com>
2023-09-13 09:34:10 +0200

    boton cerrar sesion

--- inicio-sesion/components/InputInicioSesion.jsx
+++ inicio-sesion/components/InputInicioSesion.jsx
@@ -2,22 +2,35 @@ import React, { useState} from 'react'
 
 const InputInicioSesion = () => {
 
-    const [datosUsuario, setDatosUsuario] = useState()
+    const [datosUsuario, setDatosUsuario] = useState({})
 
-    const [datosLocal, setDatosLocal] = useState([])
-
+    // ---------guardar datos
     const handleGuardarDatos = () => {
-        localStorage.setItem("nombre usuario", JSON.stringify(datosUsuario))
+        localStorage.setItem("datos formulario", JSON.stringify(datosUsuario))
+        setDatosUsuario("")
+
+    }
+
+    // ---------devolver datos
+    const handleSacarDatos = () => {
+        const datosLocal = localStorage.getItem("datos formulario")
+        setDatosUsuario(JSON.parse(datosLocal))
+    }
+
+    //--------limpiar datos
+    const handleCerrarSesion = () => {
+        localStorage.removeItem("datos formulario")
     }
 
   return (
     <>
     <div>Inicia sesión: </div>
-        {datosUsuario}
         <br></br>
-        <input type="text" onChange={(e)=>{setDatosUsuario(e.target.value)}}></input>
+        <input type="text" onChange={(e)=>{setDatosUsuario({...datosUsuario,nombre:e.target.value})}} value={datosUsuario.nombre ?? ""}></input>
+        <input type='password' onChange={(e)=>{setDatosUsuario({...datosUsuario,password:e.target.value})}} value={datosUsuario.password ?? ""}></input>
         <button onClick={()=>{handleGuardarDatos()}}>INICIAR SESIÓN</button>
-        <button onClick={()=>{}}>Recuperar sesión</button>
+        <button onClick={()=>{handleSacarDatos()}}>Recuperar sesión</button>
+        <button onClick={()=>{handleCerrarSesion()}}>Cerrar sesión</button>
     </>
   )
 }
