023f02cf41c41a785028f015c1abfacc69eb579a
Aluon <paulacanoroman@gmail.com>
2023-09-13 09:16:19 +0200

    feat: guardar datos en local

--- inicio-sesion/components/InputInicioSesion.jsx
+++ inicio-sesion/components/InputInicioSesion.jsx
@@ -2,8 +2,13 @@ import React, { useState} from 'react'
 
 const InputInicioSesion = () => {
 
-    const [datosUsuario, setDatosUsuario] = useState([])
+    const [datosUsuario, setDatosUsuario] = useState()
 
+    const [datosLocal, setDatosLocal] = useState([])
+
+    const handleGuardarDatos = () => {
+        localStorage.setItem("nombre usuario", JSON.stringify(datosUsuario))
+    }
 
   return (
     <>
@@ -11,6 +16,8 @@ const InputInicioSesion = () => {
         {datosUsuario}
         <br></br>
         <input type="text" onChange={(e)=>{setDatosUsuario(e.target.value)}}></input>
+        <button onClick={()=>{handleGuardarDatos()}}>INICIAR SESIÓN</button>
+        <button onClick={()=>{}}>Recuperar sesión</button>
     </>
   )
 }
